87835bdca6ed77e701a789f87f9175a848fff41e
MacManley <118170140+MacManley@users.noreply.github.com>
2026-06-05 23:36:00 +0100

    fix commit 5e4f878

--- examples/simpleSerialPrint/simpleSerialPrint.ino
+++ examples/simpleSerialPrint/simpleSerialPrint.ino
@@ -28,3 +28,23 @@ void loop()
 
 void startWiFi()
 {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, Password);
+  Serial.print("Attempting to connect to "); 
+  Serial.println(SSID);
+ 
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(250);
+ 
+    if ((++i % 16) == 0)
+    {
+      Serial.println(F(" still trying to connect"));
+    }
+  }
+ 
+  Serial.print(F("Connection Successful | IP Address: "));
+  Serial.println(WiFi.localIP());
+}
